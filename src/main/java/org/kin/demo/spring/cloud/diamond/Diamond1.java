c2d890fd49a7de1d0a9db3edcd759aeee0d17495
huangjianqin <18814127639@163.com>
2020-07-18 23:39:05 +0800

    Hystrix学习

--- src/main/java/org/kin/demo/spring/cloud/diamond/Diamond1.java
+++ src/main/java/org/kin/demo/spring/cloud/diamond/Diamond1.java
@@ -3,6 +3,8 @@ package org.kin.demo.spring.cloud.diamond;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * 注册中心1, 注册中心相互注册
@@ -10,6 +12,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author huangjianqin
  * @date 2020-07-17
  */
+@EnableTurbine
+@EnableHystrixDashboard
 @EnableEurekaServer
 @SpringCloudApplication
 public class Diamond1 {
