54abd5b2694b9c8025fa168b239807fea91dbe59
huangjianqin <18814127639@163.com>
2020-07-19 19:24:32 +0800

    spring cloud config学习

---
+++ src/main/java/org/kin/demo/spring/cloud/config/ConfigCenter.java
@@ -0,0 +1,17 @@
+package org.kin.demo.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author huangjianqin
+ * @date 2020-07-19
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigCenter {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenter.class, args);
+    }
+}
