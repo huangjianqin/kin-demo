296c6996870d1d79be8b4851d216e939c7344d0d
huangjianqin <18814127639@163.com>
2021-01-09 00:38:39 +0800

    spring batch 分区task demo

--- kin-demo-spring/batch/src/main/java/org/kin/demo/spring/batch/JobConfiguration1.java
+++ kin-demo-spring/batch/src/main/java/org/kin/demo/spring/batch/JobConfiguration1.java
@@ -1,18 +1,25 @@
 package org.kin.demo.spring.batch;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -20,7 +27,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @author huangjianqin
@@ -44,7 +51,7 @@ public class JobConfiguration1 {
         FlatFileItemWriter<Integer> writer = new FlatFileItemWriterBuilder<Integer>()
                 .name("writer")
                 .resource(new FileSystemResource(
-                        "out.txt"))
+                        "outs/out.txt"))
                 .lineAggregator(new PassThroughLineAggregator<>())
                 .build();
 
@@ -63,6 +70,72 @@ public class JobConfiguration1 {
                 .throttleLimit(2)
                 .build();
         return jobBuilderFactory.get("job1").start(step1).build();
+    }
+
+    @Bean
+    public Job partitionJob() {
+        Step step2 = stepBuilderFactory.get("step2")
+                .partitioner("step3", new Partitioner() {
+                    @Override
+                    public Map<String, ExecutionContext> partition(int gridSize) {
+                        //自定义分区逻辑
+                        Map<String, ExecutionContext> result = new HashMap<>(gridSize);
+                        int range = 100;
+                        for (int i = 0; i < gridSize; i++) {
+                            ExecutionContext executionContext = new ExecutionContext();
+                            executionContext.putInt("fromId", i * range);
+                            executionContext.putInt("toId", (i + 1) * range);
+
+                            result.put("partition" + i, executionContext);
+                        }
+                        return result;
+                    }
+                })
+                .step(partitionStep())
+                .gridSize(5)
+                .build();
+        return jobBuilderFactory.get("job2").start(step2).build();
+    }
+
+    @Bean
+    public Step partitionStep() {
+        return stepBuilderFactory.get("step3")
+                //commit num
+                .<Integer, Integer>chunk(33)
+                .reader(partitionReader(0, 0))
+                .writer(partitionWriter(0))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemWriter<Integer> partitionWriter(@Value("#{stepExecutionContext[fromId]}") final int fromId) {
+        //按分区写入文件
+        FlatFileItemWriter<Integer> writer = new FlatFileItemWriterBuilder<Integer>()
+                .name("writer")
+                .resource(new FileSystemResource(
+                        "outs/out" + fromId + ".txt"))
+                .lineAggregator(new PassThroughLineAggregator<>())
+                .build();
+        //!!!!! bug, 通过@StepScope 注入, FlatFileItemWriter不会调用open()方法, 这个硬调用
+        writer.open(new ExecutionContext());
+        return writer;
+    }
+
+    /**
+     * @StepScope 标识step范围内的bean
+     * #{stepExecutionContext[fromId]}  获取stepExecutionContext的某个值
+     */
+    @Bean
+    @StepScope
+    public ItemReader<Integer> partitionReader(@Value("#{stepExecutionContext[fromId]}") final int fromId,
+                                               @Value("#{stepExecutionContext[toId]}") final int toId) {
+        //模拟分区数据返回
+        List<Integer> sources = new ArrayList<>(toId - fromId + 1);
+        for (int i = fromId; i < toId; i++) {
+            sources.add(i);
+        }
+        return new ListItemReader<>(sources);
     }
 
     public static void main(String[] args) throws InterruptedException {
